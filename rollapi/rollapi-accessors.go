aba951f7720c1ac935c5c272b97bc07b2423c3c3
DJ <davidji99@users.noreply.github.com>
2020-02-10 12:26:10 +0900

    Implement PagerDuty Notification API: - Additionally, fix validation when no access/project token is passed. - Add logic to handle nil structs when PUT, PATCH, and POST.

--- rollapi/rollapi-accessors.go
+++ rollapi/rollapi-accessors.go
@@ -139,6 +139,25 @@ func (p *PATUpdateRequest) GetRateLimitWindowSize() int {
 	return *p.RateLimitWindowSize
 }
 
+// GetConfig returns the Config field.
+func (p *PDRuleRequest) GetConfig() *PDRuleConfig {
+	if p == nil {
+		return nil
+	}
+	return p.Config
+}
+
+// HasFilters checks if PDRuleRequest has any Filters.
+func (p *PDRuleRequest) HasFilters() bool {
+	if p == nil || p.Filters == nil {
+		return false
+	}
+	if len(p.Filters) == 0 {
+		return false
+	}
+	return true
+}
+
 // GetAccountID returns the AccountID field if it's non-nil, zero value otherwise.
 func (p *Project) GetAccountID() int64 {
 	if p == nil || p.AccountID == nil {
