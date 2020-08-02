5830b331119dbeb5c28b214f6160379e380c7984
DJ <davidji99@users.noreply.github.com>
2020-08-02 11:06:37 +0900

    fix deprecation of terraform.ResourceProvider

--- rollbar/provider_test.go
+++ rollbar/provider_test.go
@@ -3,30 +3,29 @@ package rollbar
 import (
 	helper "github.com/davidji99/terraform-provider-rollbar/helper/test"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"testing"
 )
 
-var testAccProviders map[string]terraform.ResourceProvider
+var testAccProviders map[string]*schema.Provider
 var testAccProvider *schema.Provider
 var testAccConfig *helper.TestConfig
 
 func init() {
-	testAccProvider = Provider().(*schema.Provider)
-	testAccProviders = map[string]terraform.ResourceProvider{
+	testAccProvider = Provider()
+	testAccProviders = map[string]*schema.Provider{
 		"rollbar": testAccProvider,
 	}
 	testAccConfig = helper.NewTestConfig()
 }
 
 func TestProvider(t *testing.T) {
-	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
+	if err := Provider().InternalValidate(); err != nil {
 		t.Fatalf("err: %s", err)
 	}
 }
 
 func TestProvider_impl(t *testing.T) {
-	var _ terraform.ResourceProvider = Provider()
+	var _ *schema.Provider = Provider()
 }
 
 func testAccPreCheck(t *testing.T) {
