b6bd3196552764e9f15613a8aeda5e9d0c4c52f9
DJ <davidji99@users.noreply.github.com>
2020-08-02 12:33:23 +0900

    bump rollrest to v0.1.3 (#30)

--- vendor/github.com/davidji99/simpleresty/simpleresty.go
+++ vendor/github.com/davidji99/simpleresty/simpleresty.go
@@ -1,8 +1,31 @@
 package simpleresty
 
-import "github.com/go-resty/resty/v2"
+import (
+	"github.com/go-resty/resty/v2"
+	"os"
+)
+
+var (
+	proxyVars = []string{"HTTPS_PROXY", "https_proxy", "HTTP_PROXY", "http_proxy"}
+)
 
 // New function creates a new SimpleResty client.
 func New() *Client {
-	return &Client{Client: resty.New()}
+	c := &Client{Client: resty.New()}
+
+	determineSetProxy(c)
+
+	return c
+}
+
+// determineSetProxy checks if any proxy variables are defined in the environment.
+// If so, set the first occurrence and exit the loop.
+func determineSetProxy(c *Client) {
+	for _, v := range proxyVars {
+		proxyUrl := os.Getenv(v)
+		if proxyUrl != "" {
+			c.SetProxy(proxyUrl)
+			break
+		}
+	}
 }
